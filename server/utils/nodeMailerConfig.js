941b0e95b5bd704dd1b692230775d021569cdd11
Mehmet Ugur Satir <musatir@gmail.com>
2023-10-18 15:01:42 +0200

    nodeMailerConfig file

---
+++ server/utils/nodeMailerConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure:false,
+    tls:{
+        rejectUnauthorized:false
+      },
+    auth: {
+        user: 'leta94@ethereal.email',
+        pass: 'kVsXRdK869H3vFKnF6'
+    }
+}
