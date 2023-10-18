c30ccf8a031a7b87937bdaef068848747115660c
Mehmet Ugur Satir <musatir@gmail.com>
2023-10-18 17:09:30 +0200

    sendVerificationEmail kinda initial setup

---
+++ server/utils/sendVerificationEmail.js
@@ -0,0 +1,12 @@
+const sendEmail = require('./sendEmail');
+
+
+
+const sendVerificationEmail = async ({name, email, verificationToken, origin})=>{
+
+    const message = `<p>Please confirm your email by clicking following link: </p>`
+
+    return sendEmail({to:email, subject:'Email Confirmation', html:`<h4>Hello ${name}</h4>
+    ${message}
+    `})
+}
